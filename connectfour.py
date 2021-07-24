1111d1b4bf7169c3b3b8da5368069d5ed3ddf621
sanzelda <thordahl04@gmail.com>
2021-07-24 22:09:50 +0200

    Added board and started checking for winners

--- connectfour.py
+++ connectfour.py
@@ -2,3 +2,17 @@
 Connect four program written by Thor.N from scratch
 Started on 7/24/2021 at 00:31
 """
+
+playing_board = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+]
+
+
+
+def check_for_winner(board):
+    pass
