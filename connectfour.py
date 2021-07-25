d2236d9bfc3d09cff5091f13e7d236557a353274
sanzelda <thordahl04@gmail.com>
2021-07-25 16:21:27 +0200

    added more functionality to check for a winner, not all yet. Also added a message for when someone wins

--- connectfour.py
+++ connectfour.py
@@ -2,6 +2,7 @@
 Connect four program written by Thor.N from scratch
 Started on 7/24/2021 at 00:31
 """
+
 import os
 
 #initial settings for cleaner game and terminal
@@ -16,12 +17,35 @@ pause = lambda: input('\nPlease press Enter to continue...')
 
 
 
-def check_for_winner(board):
-    pass
+def check_winner(board):
+    board_reversed = board[::-1] #reverses the board so it can scan and check from bottom to top
+
+    def check_rows(input_board): 
+        
+        for row in input_board:
+
+            for slot in range(len(row)):
+                if (row[slot] == 'R') and (len(set(row[slot:slot+4])) == 1):
+                    return 'R'
+                elif (row[slot] == 'Y') and (len(set(row[slot:slot+4])) == 1):
+                    return 'Y'
+
+
+    #returns who the winner is
+    if (check_rows(board_reversed) == 'R'):
+        return 'R'
+    elif (check_rows(board_reversed) == 'Y'):
+        return 'Y'
+
+
 
 
 
 def connect4():
+
+    
+
+
 
     #board that holds the game data
     playing_board = [
@@ -30,7 +54,7 @@ def connect4():
         [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
         [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
         [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ',],
+        ['Y', 'Y', 'Y', ' ', ' ', ' ', ' ',],
     ]
 
 
@@ -40,9 +64,16 @@ def connect4():
     pause()
     clear()
 
-    winner = False
+    winner = ''
     turn = 'R'
-    while (winner == False):
+    while (winner == ''):
+        
+        #checks for a winner
+        if (check_winner(playing_board) == 'R') or (check_winner(playing_board) == 'Y'):
+            winner = check_winner(playing_board)
+            break
+
+
 
         #displays who is currently playing
         if (turn == 'R'):
@@ -74,13 +105,12 @@ def connect4():
         print('| ' + playing_board[5][0] + ' | ' + playing_board[5][1] + ' | ' + playing_board[5][2] + ' | ' + playing_board[5][3] + ' | ' + playing_board[5][4] + ' | ' + playing_board[5][5] + ' | ' + playing_board[5][6] + ' |')
         print(' --- --- --- --- --- --- ---')
         
-        pause()
 
         
         chosen_slot = int(input('Pick a slot number to enter your token: ') or "0")
-            
         
         clear()
+
         slots = '1234567'
         #starts process of placing the token
         if (str(chosen_slot) in slots) and (chosen_slot):
@@ -101,11 +131,24 @@ def connect4():
             continue
         pause()
 
-        #switches turn
-        if (turn == 'R'):
-            turn = 'Y'
-        elif (turn == 'Y'):
-            turn = 'R'
+        #switches turn if there is no winner
+        if (winner == ''):
+            
+            if (turn == 'R'):
+                turn = 'Y'
+            elif (turn == 'Y'):
+                turn = 'R'
+
+    #prints the winner message
+    print(' ')
+    if (winner == 'R'):
+        print('Congrats, the winner of the game is RED!!!')
+    elif(winner == 'Y'):
+        print('Congrats, the winner of the game is YELLOW!!!')
+    elif (winner == 'draw'):
+        print('It\'s a draw!!!')
+
+
 
 connect4()
 pause()
